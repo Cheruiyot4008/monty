7141259a796f6be817c573ead658ea0fb46f3dec
cheruiyot4008 <cheruiyotbrian454@gmail.com>
2023-12-15 07:11:44 -0800

    cheruiyot

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "monty.h"
+#include <stdio.h>
+
+
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -30,7 +34,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		read_line =getline (&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
